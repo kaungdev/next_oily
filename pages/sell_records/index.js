2a4074eaf5ebe43a4a69cbef2f7c5fe0966a37e0
Kaung Myat Hein <kmh.sorcerer@gmail.com>
2019-03-11 21:38:58 +0630

    sell records finished

---
+++ pages/sell_records/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import ReactTable from "react-table";
+
+import api from "../../src/api/";
+import Layout from "../../components/Layout";
+export default class index extends Component {
+  static async getInitialProps() {
+    const oilRecords = await api.getOilSell();
+    return { serverResponse: { ...oilRecords } };
+  }
+
+  state = { data: [] };
+
+  componentDidMount() {
+    this.setState({
+      data: this.props.serverResponse.data.oilSellVouchers
+    });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Grid container>
+          <Grid item xs={12}>
+            <ReactTable
+              data={this.state.data}
+              columns={[
+                { Header: "Total Price", accessor: "totalAmount" },
+                { Header: "Month", accessor: "month" },
+                { Header: "Year", accessor: "year" }
+              ]}
+              SubComponent={row => {
+                return (
+                  <ReactTable
+                    pageSize={3}
+                    data={row.original.oilSells}
+                    columns={[
+                      { Header: "Name", accessor: "oil.name" },
+                      { Header: "Quantity", accessor: "quantity" },
+                      { Header: "Sold Price", accessor: "sellPrice" },
+                      { Header: "Total Amount", accessor: "totalAmount" }
+                    ]}
+                  />
+                );
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Layout>
+    );
+  }
+}
